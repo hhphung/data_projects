3a4ba4887690cc7b69ea788046e9591d641bafb2
hoi phung <hhphung@iastate.edu>
2024-01-21 21:59:41 -0600

    hoi

--- Docker_pipeline/upload_data.py
+++ Docker_pipeline/upload_data.py
@@ -1,16 +1,31 @@
 
+import argparse
+
+import time as time
 import pandas as pd
 from sqlalchemy import create_engine
 
 
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+
+args = parser.parse_args()
+print(args.accumulate(args.integers))
+
+
+
+
+engine = create_engine('postgresql://root:Vietnam#1@localhost:5432/LOCAL')
 
 df = pd.read_csv('./yellow_tripdata_2023-01.csv', nrows=100)
 
 
 
 
-
-engine = create_engine('postgresql://root:Vietnam#1@localhost:5432/LOCAL')
 df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
 df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
